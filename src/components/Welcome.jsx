ac46465730ea0dbfbe4b8ff493592f1eb212395f
Lee Charlton <Leejcharlton@hotmail.com>
2024-08-22 09:11:07 +0100

    Add max height to welcome container

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,11 +5,10 @@ const Welcome = () => {
     <div className="content-container welcome-container" id="welcome">
       <div className="welcome-text-container">
         <h1 className="welcome-heading load-hidden">Hi, im
-        <span className="welcome-container--blue"> Lee.</span>
+          <span className="welcome-container--blue"> Lee.</span>
         </h1>
         <h1 className="welcome-text load-hidden">I design and build attractive websites to help your business grow</h1>
       </div>
-        {/* <span className="welcome-container--blue"> Kent, United Kingdom</span> */}
       <div className="welcome-buttons-container">
         <div className="welcome-button-container reveal-button-1 load-hidden">
           <a className="cta-btn" href="#about">
