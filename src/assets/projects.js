86aaf2ea2155c0c692a0d5efc6904b6d85e19d94
Lee <leejcharlton@hotmail.com>
2025-11-06 13:22:39 +0000

    Add pinball app. Optimize all image and create a 2x dpi image for each image.

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,40 +1,51 @@
-import blogImage from "./images/blog.jpg"
-import expenseTrackerImage from "./images/expense-tracker-screenshot.png"
-import sCharltonImage from "./images/s-charlton-screenshot.webp"
-import papermillImage from "./images/papermill.jpg"
+import blogImage from './images/blog.jpg'
+import blogImage2x from './images/blog-2x.jpg'
+
+import sCharltonImage from './images/s-charlton-screenshot.jpg'
+import sCharltonImage2x from './images/s-charlton-screenshot-2x.jpg'
+
+import papermillImage from './images/papermill.jpg'
+import papermillImage2x from './images/papermill-2x.jpg'
+
+import pinballImage from './images/pinball-screenshot.png'
+import pinballImage2x from './images/pinball-screenshot-2x.png'
 
 const projects = [
   {
-    title: "Local Micropub Website - The Papermill",
+    title: 'Local Micropub Website - The Papermill',
     textContent:
-      "A website for The Papermill, a local micropub, featuring opening hours, a gallery, and contact details. It also lets the owners manage and update their beers and events to keep customers informed.",
+      'A website for The Papermill, a local micropub, featuring opening hours, a gallery, and contact details. It also lets the owners manage and update their beers and events to keep customers informed.',
     image: papermillImage,
-    liveSite: "https://pub.charltonwebdev.com/",
-    altText: "A screenshot of a micropub website.",
+    image2x: papermillImage2x,
+    liveSite: 'https://pub.charltonwebdev.com/',
+    altText: 'A screenshot of a micropub website.',
   },
   {
-    title: "S. Charlton Builders - Local Construction Specialists",
+    title: 'Pinball Club Leaderboard App',
     textContent:
-      "This static website was created for a local construction firm to highlight their expertise and showcase completed projects. It provides clear information about the services offered, along with easy access to contact details and social media links.",
+      'A web app designed for a local pinball club to track player scores and maintain competitive rankings. Features include secure user authentication, score submission, and dynamic leaderboards that encourage friendly competition. A test database has been set up specifically for portfolio visitors so feel free to create an account and submit scores to explore the full functionality!',
+    image: pinballImage,
+    image2x: pinballImage2x,
+    liveSite: 'https://pinball.charltonwebdev.com/',
+    altText: 'Screenshot of the pinball club leaderboard application.',
+  },
+  {
+    title: 'S. Charlton Builders - Local Construction Specialists',
+    textContent:
+      'This static website was created for a local construction firm to highlight their expertise and showcase completed projects. It provides clear information about the services offered, along with easy access to contact details and social media links.',
     image: sCharltonImage,
-    liveSite: "https://s-charlton-builders.charltonwebdev.com",
-    altText: "A screenshot of a construction company website.",
+    image2x: sCharltonImage2x,
+    liveSite: 'https://s-charlton-builders.charltonwebdev.com',
+    altText: 'A screenshot of a construction company website.',
   },
   {
-    title: "Travel blog",
+    title: 'Travel blog',
     textContent:
-      "A dynamic website built with a content management system, giving the user full control to publish, update, and manage their own travel stories and photos.",
+      'A dynamic website built with a content management system, giving the user full control to publish, update, and manage their own travel stories and photos.',
     image: blogImage,
-    liveSite: "https://travel.charltonwebdev.com/",
-    altText: "A screenshot of a travel blog website.",
-  },
-  {
-    title: "Expense tracker app",
-    textContent:
-      "A simple web application built to demonstrate core development skills. Users can log in securely, record their expenses, update or remove entries, and access their saved data on return.",
-    image: expenseTrackerImage,
-    liveSite: "http://expensify.charltonwebdev.com",
-    altText: "A screenshot of an expense tracker app.",
+    image2x: blogImage2x,
+    liveSite: 'https://travel.charltonwebdev.com/',
+    altText: 'A screenshot of a travel blog website.',
   },
 ]
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,28 +1,32 @@
-const Project = ({ textContent, image, title, liveSite, altText }) => {
+const Project = ({ textContent, image, image2x, title, liveSite, altText }) => {
   return (
-    <div className="project__container">
-      <div className="project__info-container load-hidden">
-        <h3 className="">{title}</h3>
+    <div className='project__container'>
+      <div className='project__info-container load-hidden'>
+        <h3 className=''>{title}</h3>
         <p>{textContent}</p>
 
-        <div className="project__button-container">
-          <a
-            href={liveSite}
-            target="_blank"
-            className="cta-btn see-live-button"
-          >
+        <div className='project__button-container'>
+          <a href={liveSite} target='_blank' className='cta-btn see-live-button'>
             See Live
           </a>
         </div>
       </div>
 
-      <div className="project__img-container load-hidden">
-        <a href={liveSite} target="_blank">
-          <img className="project__img" src={image} alt={altText}/>
+      <div className='project__img-container load-hidden'>
+        <a href={liveSite} target='_blank'>
+          <img
+            className='project__img'
+            src={image}
+            srcSet={`${image2x ?? image} 2x`}
+            alt={altText}
+            width='860'
+            height='490'
+            loading='lazy'
+          />
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Project;
+export default Project
